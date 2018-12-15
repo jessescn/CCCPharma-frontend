e08d8ef128cfe88280c86d86e636f7f489f4233d
Luan <jadsonluan1@gmail.com>
2018-12-15 00:13:06 -0300

    Implementa mudança de design de relatorio de produtos

---
+++ relatorios/produtos/index.js
@@ -0,0 +1,12 @@
+import {addProduct, getProduct, products} from '../services/productsService.js';
+
+let all_products = [];
+
+products().then(function(data) {
+    all_products = data;
+    populate();
+});
+
+function populate() {
+    
+}
