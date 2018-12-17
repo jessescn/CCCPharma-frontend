ebd66b3ce67bc643dd25fd172076c0f7d48daf6e
Mikaelbfaa <mikael.amaral@ccc.ufcg.edu.br>
2018-12-17 00:35:00 -0300

    Implementando relatório geral

--- geral/relatorio.js
+++ geral/relatorio.js
@@ -7,8 +7,7 @@ let all_orders = [];
 function init() {
     verifyPermission();
     setupListeners();
-    orderService.orders().then(function (data) {
-        console.log(data);
+    orderService.ordersInfo().then(function (data) {
         all_orders = data;
         loadOrders();
     });
@@ -16,7 +15,7 @@ function init() {
 
 function loadOrders() {
     all_orders.forEach(function (order) {
-        setProducts(order.orderProducts);
+        setProducts(order);
     });
 }
 
@@ -40,73 +39,25 @@ function sendTo(url) {
     window.location.href = "/"
 }
 
-function setProducts(products) {
-    products.forEach(function (product) {
-        let prices = getAmount(product.product.id, products);
-        let available = "Indisponível";
+function setProducts(orderInfo) {
+    let available = "Indisponível";
 
-        if (product.product.available) {
-            available = "Disponível";
-        }
+    if (orderInfo.product.available) {
+        available = "Disponível";
+    }
 
-        let nSale = prices[0];
-        let income = prices[1];
+    let template = `
+    <div class = "linha">
+        <span>${orderInfo.product.name}</span>
+        <span>${orderInfo.product.amount}</span>
+        <span>${available}</span>
+        <span>${orderInfo.quantity}</span>
+        <span>R$ ${orderInfo.totalPrice}</span>
+    </div>
+    `
 
-        let template = `
-        <div class = "linha">
-            <span>${product.product.name}</span>
-            <span>${product.product.amount}</span>
-            <span>${available}</span>
-            <span>${nSale}</span>
-            <span>R$ ${income}</span>
-        </div>
-        `
-
-        let $line = $(template);
-        $line.appendTo($orders);
-    })
+    let $line = $(template);
+    $line.appendTo($orders);
 }
-
-// function isAvailable(product) {
-//     if(product.product.available) {
-//         return "Disponível";
-//     } else {
-//         return "Indisponível";
-//     }
-// }
-
-function getAmount(id, orders) {
-    let salesData = [0, 0];
-    let ident = id;
-
-    orders.forEach(function (element) {
-        if (element.product.id == ident) {
-            salesData[0] += element.quantity;
-            salesData[1] += element.totalPrice;
-        }
-    });
-
-    return salesData;
-}
-
-// function generateLine(product) {
-//     let prices = getAmount(product.product.id, products);
-//     let nSale = prices[0];
-//     let income = prices[1];
-
-//     let template = `
-//         <div class = "linha">
-//             <span>${product.product.name}</span>
-//             <span>${product.product.amount}</span>
-//             <span>${product.product.available}</span>
-//             <span>${nSale}</span>
-//             <span>R$ ${income}</span>
-//         </div>
-//         `
-
-//     let $line = $(template);
-//     $line.appendTo($orders);
-// }
-
 
 init();
